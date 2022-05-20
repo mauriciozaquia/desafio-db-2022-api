678cdf5d124beaf0e249959923a1242fade70851
mauriciozaquia <mauricio.zaquia@hotmail.com>
2022-05-19 23:24:37 -0300

    Adicionado teste de contrato

---
+++ src/test/java/Test/Extra/PostCriarContatoValidarContrato.java
@@ -0,0 +1,32 @@
+package Test.Extra;
+
+import TestBase.CriarContatoTestBase;
+import Utilities.FileOperations;
+import io.restassured.module.jsv.JsonSchemaValidator;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+
+public class PostCriarContatoValidarContrato extends CriarContatoTestBase {
+
+    @Test
+    public void criarContatoValidandoContrato() {
+        Response payLoad =
+                given()
+                        .spec(requestSpec)
+                        .log().all()
+                .when()
+                        .post()
+                .then()
+                        .log().all()
+                        .spec(responseSpec).extract().response()
+                ;
+
+        //FileOperations.setProperties("contact","id", id);
+        id = payLoad.then().extract().path("data.id");
+        payLoad.then().body(JsonSchemaValidator.matchesJsonSchemaInClasspath("Schemas/PostCriarContatoJsonSchema.json"));
+    }
+
+}
